89ca0cd2619b002768d9947dd17f1b8e388f38db
Brian Boucher <bmboucher@gmail.com>
2021-12-15 12:13:30 -0500

    Adding city labels

--- python/src/pyrailbaron/map/draw.py
+++ python/src/pyrailbaron/map/draw.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from typing import List, Callable, Dict
 import json
+import csv
 
 from pyrailbaron.map.states import get_border_data, get_region_border_points
 from pyrailbaron.map.svg import MapSvg, MapSvgLayer, transform_lcc, transform_dxf
@@ -41,9 +42,13 @@ def main(root_dir):
     labels = svg.layer('labels')
     rr_layers: Dict[str, MapSvgLayer] = {}
     for p in map.points:
-        holes.circle(p.final_svg_coords, 2.25, stroke='blue', stroke_width=0.5,
-            fill = 'none' if len(p.city_names) == 0 else 'black')
-        labels.text(str(p.index), p.final_svg_coords, font_size='5px')
+        holes.circle(p.final_svg_coords, 2.6, stroke='blue', stroke_width=0.01,
+            fill = 'none')
+        if len(p.city_names) > 0:
+            #labels.text(p.place_name.upper(), p.final_svg_coords, font_size='5px')
+            pass
+        else:
+            labels.text(str(p.index), p.final_svg_coords, font_size='4px')
         for rr in p.connections:
             if rr not in rr_layers:
                 rr_layers[rr] = svg.layer(f'rr_{rr}')
@@ -52,17 +57,33 @@ def main(root_dir):
                     other_p = map.points[j]
                     rr_layers[rr].line(p.final_svg_coords, other_p.final_svg_coords,
                         stroke='red', stroke_width=1.0)
-    
+
     cities = svg.layer('cities')
     for p in map.points:
         if len(p.city_names) == 0:
             cities.circle(p.final_svg_coords, 1.5, fill='white', stroke='black',
                 stroke_width=1, stroke_linecap='round', stroke_linejoin='round')
         else:
-            cities.text(p.place_name.upper(), p.final_svg_coords,
-                font_size='5px')
             cities.square(p.final_svg_coords, 8, 0, fill='white', stroke='black',
                 stroke_width=1.5, stroke_linecap='round', stroke_linejoin='round')
+
+    labels2 = svg.layer('labels2')
+    with (root_dir / 'data/svg_text.csv').open('rt') as svg_text_file:
+        svg_text = list(csv.reader(svg_text_file))
+    for text, posType, x_s, y_s in svg_text:
+        x,y = float(x_s), float(y_s)
+        if posType == 'c':
+            try:
+                search_text = text.replace(r'\n','').replace(' ','').upper()
+                city_pt = next(p for p in map.points 
+                    if p.place_name.upper().replace(' ','') == search_text
+                    and len(p.city_names) > 0)
+                city_x, city_y = city_pt.final_svg_coords
+                for line in text.split(r'\n'):
+                    labels2.text(line.replace('_',''), (city_x + x, city_y + y), font_size='4px', font_family='Verdana')
+                    y += 4
+            except StopIteration:
+                print(f'Could not find city for {text}')
 
     svg.save()
 
